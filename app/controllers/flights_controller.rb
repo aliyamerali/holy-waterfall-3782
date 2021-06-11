a01e1ca1a172c2587e4456ba24f202046849f5ad
Aliya Merali <merali4@gmail.com>
2021-06-11 09:39:08 -0600

    Add route, view, controller action for US2 button functionality

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,5 +3,13 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
   end
-  
+
+  def remove_passenger
+    flight = Flight.find(params[:flight_id])
+    passenger = Passenger.find(params[:pass_id])
+
+    flight.passengers.delete(passenger)
+
+    redirect_to '/flights'
+  end
 end
--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -12,9 +12,12 @@ RSpec.describe 'Flights Index Page', type: :feature do
     @flight5 = @frontier.flights.create!(number: "9872", date: "09/05/20", departure_city: "Los Angeles", arrival_city: "Miami")
     @flight6 = @frontier.flights.create!(number: "2341", date: "09/15/20", departure_city: "Chicago", arrival_city: "Houston")
 
-    @pass1a = @flight1.passengers.create!(name: "Joe", age: 7)
-    @pass1b = @flight1.passengers.create!(name: "Sam", age: 23)
-    @pass1c = @flight1.passengers.create!(name: "Lin", age: 76)
+    @pass1a = Passenger.create!(name: "Joe", age: 7)
+    Manifest.create(passenger: @pass1a, flight: @flight1)
+    @pass1b = Passenger.create!(name: "Sam", age: 23)
+    Manifest.create(passenger: @pass1b, flight: @flight1)
+    @pass1c = Passenger.create!(name: "Lin", age: 76)
+    Manifest.create(passenger: @pass1c, flight: @flight1)
 
     @pass2a = @flight2.passengers.create!(name: "Jill", age: 98)
     @pass2b = @flight2.passengers.create!(name: "Tan", age: 66)
@@ -142,6 +145,7 @@ RSpec.describe 'Flights Index Page', type: :feature do
   end
 
   it 'clicking the remove link returns flight index page without that passenger for the flight' do
+
     within(".pass-#{@pass1a.id}") do
       click_button("Remove Passenger")
     end
@@ -155,7 +159,7 @@ RSpec.describe 'Flights Index Page', type: :feature do
     end
   end
 
-  it 'clicking the remove link does notdelete the passenger record' do
+  it 'clicking the remove link does not delete the passenger record' do
     within(".pass-#{@pass1a.id}") do
       click_button("Remove Passenger")
     end
