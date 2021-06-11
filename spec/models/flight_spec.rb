3f8ee884e3e1ff6329b136d55ce6b9f517154cae
Aliya Merali <merali4@gmail.com>
2021-06-11 08:17:29 -0600

    Add model association tests

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
   it {should belong_to :airline}
+  it {should have_many :manifests}
+  it {should have_many(:passengers).through(:manifests)}
 end
